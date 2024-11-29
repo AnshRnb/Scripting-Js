adbbed04ac0b900d276101d780fb162cb176736b
AnshRnb <anshrock@gmail.com>
2024-11-30 00:33:54 +0530

    3 functions added

---
+++ java.js
@@ -0,0 +1,68 @@
+// regular function::::::)
+
+// function btn() {
+//   var name = "ansh";
+//   alert("please press okay");
+//   console.log(name);
+//   document.getElementById("show").innerHTML = name;
+// }
+
+//arrow function with just ID:::::
+
+// document.getElementById("btn").onclick = ()=>
+// {  //WITH OR WITHOUT {}
+//   var name = "Ansh";
+//   alert("please press okay");
+//   console.log(name);
+//   document.getElementById("show").innerHTML = name;
+// }
+
+// ------------
+
+// var btn = ()=> //with just onclick on the html button:::)
+// document.getElementById("show").innerHTML = "Ansh:)"
+
+// const val = (a)=> a * a;
+// const val = (a,b)=> a * b;
+// const val = (a,b)=> a + b;
+
+
+
+
+// function expression::::
+
+// const val = function(a,b)
+// {
+//      return a - b;
+// } 
+
+// with arrow:::
+
+// const val = (a,b) =>
+// {
+//      return a - b;
+// } 
+
+
+
+//function declaration::
+
+// function ansh()
+// {
+//     document.write("ansh")
+// }
+// ansh();  //call
+
+
+
+
+
+// -----------event-listener
+
+// var nameo = document.getElementById("btn");
+// nameo.addEventListener("click", call);
+
+// function call() {
+//   alert("hey i am ansh");
+//   document.getElementById("show").textContent = "HELLO";
+// }
